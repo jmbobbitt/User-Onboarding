22afdc689103330fefb206660ed1a313211091d1
Jenni B <71854518+jmbobbitt@users.noreply.github.com>
2021-03-21 19:14:12 -0400

    completed mvp

--- user-onboarding/src/Components/Users.js
+++ user-onboarding/src/Components/Users.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Users - (props) => {
+export const Users = (props) => {
     const {user} = props;
     return (
         <h1>{user.map(e += <h1>{e.name}</h1>)}</h1>
